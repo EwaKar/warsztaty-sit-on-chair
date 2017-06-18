63d50ff2fc9aad8bfb719f71039abbef1dc39af6
Imię igit config --global user.name Imię <ekarbowska@pak.torun.pl>
2017-06-18 13:34:03 +0200

    nie mogę zaimplementować dropdown list, uwazam, że kod powinnien działać, a nie działa

--- js/app.js
+++ js/app.js
@@ -1,31 +1,54 @@
 //nie działa button nextPicture od razu, tzn trzeba najpierw użyć buttona "prevPicture", a później dopiero "nextPicture" :()
 //nie mogę wypisać ID aktualnego zdjęcia
 document.addEventListener("DOMContentLoaded", function() {
+// invisible
 
-var lis = document.querySelector("nav>ul>li");
-//var lislis = document.querySelectorAll("nav>ul>li>ul");
-var lislis = document.querySelectorAll(".invisible");
 
-  lislis.forEach(function(e) {
-  e.style.display = "none";
+var infinitive = document.querySelectorAll(".infinitive")
+var toHide = document.querySelectorAll(".layer-7")
+
+for (var i = 0; i < infinitive.length; i++) {
+
+infinitive[i].addEventListener("mouseover", function() {
+  var toHide = this.firstElementChild;
+  if (toHide) {
+    toHide.style.display = "none";
+  }
 });
-
-for (var i = 0; i < lis.length; i++) {
-  lis[i].addEventListener("mouseover", function() {
-    var lisLis = this.firstElementChild;
-    if (lisLis) {
-      lisLis.style.display = "block";
-    }
-  });
-  lis[i].addEventListener("mouseout", function() {
-    var lisLis = this.firstElementChild;
-    if (lisLis) {
-      lisLis.style.display = "none";
-    }
-  });
+infinitive[i].addEventListener("mouseout", function() {
+  var toHide = this.firstElementChild;
+  if (toHide) {
+    toHide.style.display = "block";
+  }
+});
 }
 
 
+
+
+//menu
+  var lis = document.querySelectorAll("nav>ul>li");
+
+  var lislis = document.querySelectorAll("nav>ul>li>ul");
+    lislis.forEach(function(e) {
+    e.style.display = "none";
+  });
+
+  for (var i = 0; i < lis.length; i++) {
+
+    lis[i].addEventListener("mouseover", function() {
+      var lisLis = this.firstElementChild;
+      if (lisLis) {
+        lisLis.style.display = "block";
+      }
+    });
+    lis[i].addEventListener("mouseout", function() {
+      var lisLis = this.firstElementChild;
+      if (lisLis) {
+        lisLis.style.display = "none";
+      }
+    });
+  }
 
 //slider
 var nextPicture = document.getElementById("nextPicture");
@@ -34,7 +57,7 @@ var lis = document.querySelectorAll(".slider li");
 console.log(lis);
 var current = 0;
 
-nextPicture.addEventListener("click", function () {
+prevPicture.addEventListener("click", function () {
 console.log('click nextPicture');
 lis[current].classList.remove('visible');
 current = current - 1;
@@ -48,7 +71,7 @@ if (current <= 0) {
 
 lis[current].classList.add("visible");
 
-prevPicture.addEventListener("click", function () {
+nextPicture.addEventListener("click", function () {
   console.log("click prev");
   lis[current].classList.remove("visible");
 
